f68d2c672c7f6c9dba25aabba67a890de8069758
Romitrcv <32649280+Romitrcv@users.noreply.github.com>
2019-03-06 23:41:18 +0530

    Update try3.py
    using shorting of contors

--- try3.py
+++ try3.py
@@ -19,6 +19,7 @@ X=1
 Y=1
 Xn=1
 Yn=1
+areaArray = []
 while True:
     ret,img=cam.read()
     image = img
@@ -49,18 +50,35 @@ while True:
     im2,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     if len(contours) != 0:
+        for i, c in enumerate(contours):
+            area = cv2.contourArea(c)
+            areaArray.append(area)
+            #first sort the array by area
+            sorteddata = sorted(zip(areaArray, contours), key=lambda x: x[0], reverse=True)
+
     # draw in blue the contours that were founded
       #  cv2.drawContours(output, contours, -1, 255, 0)
-
+      
     #find the biggest area
         c = max(contours, key = cv2.contourArea)
 
-        x,y,w,h = cv2.boundingRect(c)
-    # draw the book contour (in green)
-    
+        x,y,w,h = cv2.boundingRect(c)        
+    # draw the first rectangle (in green)    
         cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
         cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+        #find the nth largest contour [n-1][1], in this case 2
+ #       first = sorteddata[0][1]
+        second = sorteddata[1][1]       
 
+  #      x,y,w,h = cv2.boundingRect(first)        
+    # draw the first rectangle (in green)    
+   #     cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
+    #    cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+
+        x,y,w,h = cv2.boundingRect(second)        
+    # draw the first rectangle (in green)    
+        cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
 
  # show the images   
     cv2.imshow("Result", np.hstack([image, output]))
